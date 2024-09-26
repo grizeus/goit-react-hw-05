0c1da5681b27fd73e6d26e4688103424d8f98ad0
grizeus <zaytsewww@gmail.com>
2024-09-26 16:16:35 +0300

    .

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,28 +1,35 @@
 import styles from "./MovieList.module.css";
 
 const MovieList = props => {
-    const { movies, actors, id, idHandler, resetter } = props;
-    
-    
+  const { movies, actors, id, idHandler, resetter } = props;
 
   return (
     <ul className={styles.list}>
       {movies.map(movie => (
-        <li key={movie.id} onMouseEnter={() => idHandler(movie.id)} onMouseLeave={resetter}>
-          <div className={styles.thumbnail}>
-            <img className={styles.image}
+        <li key={movie.id}>
+          <div
+            className={styles.thumbnail}
+            onMouseEnter={() => idHandler(movie.id)}
+            onMouseLeave={resetter}>
+            <img
+              className={styles.image}
               src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
               alt={movie.title}
             />
           </div>
-          <h3>{movie.title}</h3>
           {actors.length > 0 && id === movie.id && (
-            <ul>
-              {actors.slice(0,5).map((actor) => (
-                <li key={actor.id}>{actor.name}</li>
-              ))}
-            </ul>
+            <div style={{ position: "relative" }}>
+              <ul className={styles["top-cast"]}>
+                {actors.slice(0, 5).map(actor => (
+                    <li className={styles.actor} key={actor.id}>
+                        <span>{actor.name}</span>
+                        <span>{actor.character}</span>
+                    </li>
+                ))}
+              </ul>
+            </div>
           )}
+          <h3>{movie.title}</h3>
         </li>
       ))}
     </ul>
