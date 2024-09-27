dbabdbc8303aa298fd62b1053b7f14d65f31c7b8
grizeus <32486511+grizeus@users.noreply.github.com>
2024-09-27 15:39:40 +0300

    add reviews logic (#6)

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { fetchReviews } from "../../api/fetch-api";
+import { useParams } from "react-router-dom";
+
+import styles from "./MovieReviews.module.css";
+
 const MovieReviews = () => {
-  return <div>Movie Reviews</div>;
+  const { movieId } = useParams();
+  const [results, setResults] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await fetchReviews(movieId);
+        setResults(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [movieId]);
+  return (
+    <ul>
+      {results.length > 0 ? (
+        results.map(review => (
+          <li className={styles.review} key={review.id}>
+            <h3 className={styles["review-title"]}>{review.author}</h3>
+            <p>{review.content}</p>
+          </li>
+        ))
+      ) : (
+        <li>No reviews yet</li>
+      )}
+    </ul>
+  );
 };
 
 export default MovieReviews;
