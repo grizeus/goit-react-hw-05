1fcf88ea70b281bdbc12dd938ae8057709a1d0d2
grizeus <zaytsewww@gmail.com>
2024-09-28 20:42:26 +0300

    add not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,17 @@
+import Container from "../../components/Container/Container";
+import Navigation from "../../components/Navigation/Navigation";
+
+import styles from "./NotFoundPage.module.css";
+
 const NotFoundPage = () => {
-  return <div>Not Found Page</div>;
+  return (
+    <>
+      <Navigation />
+      <Container>
+        <h1 className={styles.title}>404 - Page Not Found</h1>
+      </Container>
+    </>
+  );
 };
 
 export default NotFoundPage;
